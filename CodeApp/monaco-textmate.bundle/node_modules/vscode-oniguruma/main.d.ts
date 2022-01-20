cba3b007c71a109875b15e2f4ff6449faf639e3a
Ken Chung <ken.chung@thebaselab.com>
2022-01-21 02:47:22 +0800

    adopt latest `monaco-tm` (potential fix for #237)

--- CodeApp/monaco-textmate.bundle/node_modules/vscode-oniguruma/main.d.ts
+++ CodeApp/monaco-textmate.bundle/node_modules/vscode-oniguruma/main.d.ts
@@ -19,10 +19,32 @@ export class OnigString {
 	public dispose(): void;
 }
 
+export const enum FindOption {
+	None = 0,
+	/**
+	 * equivalent of ONIG_OPTION_NOT_BEGIN_STRING: (str) isn't considered as begin of string (* fail \A)
+	 */
+	NotBeginString = 1,
+	/**
+	 * equivalent of ONIG_OPTION_NOT_END_STRING: (end) isn't considered as end of string (* fail \z, \Z)
+	 */
+	NotEndString = 2,
+	/**
+	 * equivalent of ONIG_OPTION_NOT_BEGIN_POSITION: (start) isn't considered as start position of search (* fail \G)
+	 */
+	NotBeginPosition = 4,
+	/**
+	 * used for debugging purposes.
+	 */
+	DebugCall = 8,
+}
+
 export class OnigScanner {
 	constructor(patterns: string[]);
 	public dispose(): void;
-	public findNextMatchSync(string: string | OnigString, startPosition: number, debugCall?: boolean): IOnigMatch;
+	public findNextMatchSync(string: string | OnigString, startPosition: number, options: number): IOnigMatch | null;
+	public findNextMatchSync(string: string | OnigString, startPosition: number, debugCall: boolean): IOnigMatch | null;
+	public findNextMatchSync(string: string | OnigString, startPosition: number): IOnigMatch | null;
 }
 
 export interface IOnigCaptureIndex {
