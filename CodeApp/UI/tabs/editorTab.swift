0152350d93240bf75148fed519d072c63d1524fd
Ken Chung <ken.chung@thebaselab.com>
2022-03-05 23:53:29 +0800

    UI improvements for editor tabs

--- CodeApp/UI/tabs/editorTab.swift
+++ CodeApp/UI/tabs/editorTab.swift
@@ -55,9 +55,15 @@ struct editorTab: View {
                                 onCloseEditor()
                             }
                     }else{
-                        Image(systemName: "circle.fill").font(.system(size:6)).foregroundColor(.gray).padding(.trailing, 2).onTapGesture {
-                            onCloseEditor()
-                        }
+                        Image(systemName: "circle.fill")
+                            .font(.system(size:7))
+                            .foregroundColor(Color.init(id: "tab.activeForeground"))
+                            .frame(width: 18, height: 18)
+                            .contentShape(RoundedRectangle(cornerRadius: 2, style: .continuous))
+                            .hoverEffect(.highlight)
+                            .onTapGesture{
+                                onCloseEditor()
+                            }
                     }
                     
                 }
@@ -74,9 +80,15 @@ struct editorTab: View {
                         fileIcon(url: currentEditor.url, iconSize: 12, type: currentEditor.type)
                         Text(editorDisplayName(editor: currentEditor)).lineLimit(1).font(.system(size: 13, weight: .light)).foregroundColor(Color.init(id: "tab.inactiveForeground"))
                         if currentEditor.currentVersionId != currentEditor.lastSavedVersionId {
-                            Image(systemName: "circle.fill").font(.system(size:8)).foregroundColor(Color.init(id: "tab.inactiveForeground")).padding(.trailing, 2).onTapGesture {
-                                onCloseEditor()
-                            }
+                            Image(systemName: "circle.fill")
+                                .font(.system(size:7))
+                                .foregroundColor(Color.init(id: "tab.inactiveForeground"))
+                                .frame(width: 18, height: 18)
+                                .contentShape(RoundedRectangle(cornerRadius: 2, style: .continuous))
+                                .hoverEffect(.highlight)
+                                .onTapGesture{
+                                    onCloseEditor()
+                                }
                         }
                     }
                 }.keyboardShortcut(editorTab.keyForInt(int: index+1), modifiers: .command)
