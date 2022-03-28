f37b183e8d01914a35716c69848801096e3e35bd
Ken Chung <ken.chung@thebaselab.com>
2022-03-29 00:31:08 +0800

    Add UTF-8 meta tag in html template #398

--- CodeApp/UI/file/newFile.swift
+++ CodeApp/UI/file/newFile.swift
@@ -133,6 +133,7 @@ struct newFileView: View {
             <html>
               <head>
                 <title>Title</title>
+                <meta charset="utf-8">
               </head>
               <body>
                 <h1>Hello, World</h1>
