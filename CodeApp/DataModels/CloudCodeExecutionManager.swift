9760dff71b9c6056826298e61347b7a1961ba564
Ken Chung <ken.chung@thebaselab.com>
2022-05-15 16:18:22 +0800

    Support custom Java main entry name (close #440 #432 #403)

--- CodeApp/DataModels/CloudCodeExecutionManager.swift
+++ CodeApp/DataModels/CloudCodeExecutionManager.swift
@@ -6,11 +6,12 @@
 //
 
 import SwiftUI
+import ZIPFoundation
 
 class CloudCodeExecutionManager: ObservableObject {
 
-    private static let judge0Server = "SERVER_URL"
-    private static let judge0Key = "YOUR_OWN_KEY"
+    private static let judge0Server = ""
+    private static let judge0Key = ""
 
     @Published var isRunningCode = false
     @Published var consoleContent = ""
@@ -51,7 +52,49 @@ class CloudCodeExecutionManager: ObservableObject {
 
     }
 
+    private func generateJavaParameters(sourceURL: URL) -> [String: Any] {
+
+        let archive = Archive(accessMode: .create)!
+
+        do {
+            try archive.addEntry(with: sourceURL.lastPathComponent, fileURL: sourceURL)
+
+            let tempDir = FileManager().temporaryDirectory
+            let compileFile = tempDir.appendingPathComponent("compile")
+            let runFile = tempDir.appendingPathComponent("run")
+
+            try "javac \(sourceURL.lastPathComponent)".write(
+                to: compileFile, atomically: true, encoding: .utf8)
+            try "java \(sourceURL.deletingPathExtension().lastPathComponent)".write(
+                to: runFile, atomically: true, encoding: .utf8)
+
+            try archive.addEntry(with: "compile", fileURL: compileFile)
+            try archive.addEntry(with: "run", fileURL: runFile)
+        }catch {
+            return [:]
+        }
+        
+
+        let base64 = archive.data!.base64EncodedString(options: .lineLength64Characters)
+
+        let parameters: [String: Any] = [
+            "language_id": 89,  // Multi-file program
+            "stdin": stdin.base64Encoded()!,
+            "cpu_time_limit": 6,
+            "additional_files": base64,
+        ]
+
+        return parameters
+    }
+
     func runCode(directoryURL: URL, source: String, language: Int) {
+        
+        if CloudCodeExecutionManager.judge0Server.isEmpty {
+            self.consoleContent = "Server-side execution is unsupported in TestFlight builds."
+            return
+        }
+        
+        
         self.isRunningCode = true
 
         self.consoleContent = "Running \(directoryURL.lastPathComponent) remotely..\n"
@@ -63,14 +106,17 @@ class CloudCodeExecutionManager: ObservableObject {
             )!)
         request.httpMethod = "POST"
 
-        let parameters: [String: Any] = [
-            "source_code": source.base64Encoded()!,
-            "language_id": language,
-            "stdin": stdin.base64Encoded()!,
-            "cpu_time_limit": 6,
-            "additional_files": returnDirectoryAsBase64(
-                url: directoryURL.deletingLastPathComponent(), fileURL: directoryURL),
-        ]
+        let parameters: [String: Any] =
+            language == 62
+            ? generateJavaParameters(sourceURL: directoryURL)
+            : [
+                "source_code": source.base64Encoded()!,
+                "language_id": language,
+                "stdin": stdin.base64Encoded()!,
+                "cpu_time_limit": 6,
+                "additional_files": returnDirectoryAsBase64(
+                    url: directoryURL.deletingLastPathComponent(), fileURL: directoryURL),
+            ]
 
         do {
             request.httpBody = try JSONSerialization.data(
