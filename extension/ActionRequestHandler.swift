368a86322c15e1309dac567966b235ccd9e546b5
Ken Chung <ken.chung@thebaselab.com>
2022-09-08 23:59:44 +0800

    Node.js 16.16.0 and yarn support

--- extension/ActionRequestHandler.swift
+++ extension/ActionRequestHandler.swift
@@ -99,7 +99,10 @@ class ActionRequestHandler: NSObject, NSExtensionRequestHandling {
         setenv("npm_config_prefix", sharedURL.appendingPathComponent("lib").path, 1)
         setenv("npm_config_cache", FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.path, 1)
         setenv("npm_config_userconfig", FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appendingPathComponent(".npmrc").path, 1)
-        
+        setenv("COREPACK_HOME", sharedURL.appendingPathComponent("corepack").path, 1)
+        setenv("TMPDIR", FileManager.default.temporaryDirectory.path, 1)
+        setenv("YARN_CACHE_FOLDER", FileManager.default.temporaryDirectory.path, 1)
+        setenv("HOME", sharedURL.path, 1)
         // Do not call super in an Action extension with no user interface
         self.extensionContext = context
         
