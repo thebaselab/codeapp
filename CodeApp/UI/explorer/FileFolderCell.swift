817f356df3f0be720ea5ba07645f977bf2fbce74
Ken Chung <ken.chung@thebaselab.com>
2022-09-11 01:12:17 +0800

    explorer: Support for dragging files and folders

--- CodeApp/UI/explorer/FileFolderCell.swift
+++ CodeApp/UI/explorer/FileFolderCell.swift
@@ -15,9 +15,28 @@ struct FileFolderCell: View {
         if item.subFolderItems != nil {
             FolderCell(item: item)
                 .frame(height: 16)
+                .onDrag{
+                    guard let url = item._url else {
+                        return NSItemProvider()
+                    }
+                    let itemProvider = NSItemProvider()
+                    itemProvider.suggestedName = url.lastPathComponent
+                    itemProvider.registerFileRepresentation(forTypeIdentifier: "public.folder", visibility: .all) {
+                        $0(url, false, nil)
+                        return nil
+                    }
+                    return itemProvider
+                }
         } else {
             FileCell(item: item)
                 .frame(height: 16)
+                .onDrag{
+                    guard let url = item._url, let provider = NSItemProvider(contentsOf: url) else {
+                        return NSItemProvider()
+                    }
+                    provider.suggestedName = url.lastPathComponent
+                    return provider
+                }
         }
     }
 }
