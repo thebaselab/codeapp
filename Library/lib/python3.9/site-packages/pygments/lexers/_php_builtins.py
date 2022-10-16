6043d3aed9d42dc05c60ab4fc66ed0dcb2341263
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-16 08:07:23 +0000

    Adding tarfile member sanitization to extractall()

--- Library/lib/python3.9/site-packages/pygments/lexers/_php_builtins.py
+++ Library/lib/python3.9/site-packages/pygments/lexers/_php_builtins.py
@@ -4725,7 +4725,26 @@ if __name__ == '__main__':  # pragma: no cover
     def get_php_references():
         download = urlretrieve(PHP_MANUAL_URL)
         with tarfile.open(download[0]) as tar:
-            tar.extractall()
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(tar)
         yield from glob.glob("%s%s" % (PHP_MANUAL_DIR, PHP_REFERENCE_GLOB))
         os.remove(download[0])
 
