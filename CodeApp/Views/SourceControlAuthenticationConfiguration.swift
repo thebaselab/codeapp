a661030f6d3707f3183951529341e57034c3eda4
Chung Shing Hin <sh.chung@thebaselab.com>
2023-08-17 12:00:30 +0800

    git: UI for SSH auth / multi-credentials

--- CodeApp/Views/SourceControlAuthenticationConfiguration.swift
+++ CodeApp/Views/SourceControlAuthenticationConfiguration.swift
@@ -5,15 +5,32 @@
 //  Created by Ken Chung on 2/1/2021.
 //
 
+import LocalAuthentication
 import SwiftUI
+
+func withDeviceOwnerAuthentication(titleKey: String, action: () async -> Void) async throws {
+    let title = NSLocalizedString(titleKey, comment: "")
+    let context = LAContext()
+    let authSuccess = try await context.evaluatePolicy(
+        .deviceOwnerAuthentication, localizedReason: title)
+    guard authSuccess else {
+        throw NSError(descriptionKey: "Biometric authentication failed")
+    }
+    await action()
+}
 
 struct SourceControlAuthenticationConfiguration: View {
 
     @EnvironmentObject var App: MainApp
+    @EnvironmentObject var alertManager: AlertManager
 
     @State var username: String
     @State var password: String
     @State var isDocumentationPresented: Bool = false
+    @State var sshKey: String
+    @State var isEditingSSHKey: Bool = false
+    @State var isSSHKeyConfigured: Bool
+    @State var isConfirmingSSHKeyReset: Bool = false
 
     @Environment(\.presentationMode) var presentationMode
 
@@ -28,65 +45,135 @@ struct SourceControlAuthenticationConfiguration: View {
         } else {
             _password = State(initialValue: "")
         }
+        if let sshKey = KeychainWrapper.standard.string(forKey: "git-ssh-key") {
+            _sshKey = State(initialValue: sshKey)
+            _isSSHKeyConfigured = State(initialValue: true)
+        } else {
+            _sshKey = State(initialValue: "")
+            _isSSHKeyConfigured = State(initialValue: false)
+        }
+    }
+
+    func resetSSHKey() {
+        sshKey = ""
+        isSSHKeyConfigured = false
+        KeychainWrapper.standard.removeObject(forKey: "git-ssh-key")
     }
 
     var body: some View {
         VStack {
             Form {
                 Section(
-                    header: Text("source_control.remote_credentials"),
-                    footer: VStack(alignment: .leading) {
-
-                        Text("credentials.note")
-                            .padding(.bottom, 4)
-
-                        Text("source_control.credentials.note")
-                            .padding(.bottom, 4)
-
-                        Button(action: {
-                            isDocumentationPresented.toggle()
-                        }) {
-                            Text("source_control.setup_your_credentials")
-                                .font(.footnote)
-                        }
-                    }
-
+                    header: Text("source_control.password_based_authentication")
                 ) {
-                    TextField("User Name", text: $username)
-                        .textContentType(.name)
+                    TextField("source_control.username", text: $username)
+                        .textContentType(.username)
                         .disableAutocorrection(true)
                         .autocapitalization(.none)
-                    SecureField("source_control.pat", text: $password)
+                    SecureField("source_control.pat_or_password", text: $password)
                         .textContentType(.password)
                         .disableAutocorrection(true)
                         .autocapitalization(.none)
+                }
 
+                Section("source_control.ssh_authentication") {
+                    if !isSSHKeyConfigured || isEditingSSHKey {
+                        TextEditorWithPlaceholder(
+                            placeholder:
+                                "source_control.private_key_content",
+                            text: $sshKey,
+                            customFont: .custom("Menlo", size: 13, relativeTo: .footnote)
+                        )
+                        .disableAutocorrection(true)
+                        .autocapitalization(.none)
+                    } else {
+
+                        HStack {
+                            Text("********")
+                            Spacer()
+                            Image(systemName: "eye")
+                        }
+                        .contentShape(Rectangle())
+                        .foregroundColor(.secondary)
+                        .onTapGesture {
+                            Task {
+                                try await withDeviceOwnerAuthentication(
+                                    titleKey: "source_control.editing_ssh_private_key"
+                                ) {
+                                    withAnimation {
+                                        isEditingSSHKey = true
+                                    }
+                                }
+                            }
+                        }
+                    }
+
+                    if isSSHKeyConfigured && !isEditingSSHKey {
+                        Button("common.reset", role: .destructive) {
+                            isConfirmingSSHKeyReset.toggle()
+                        }
+                        .confirmationDialog(
+                            "source_control.ssh_key_reset_confirmation",
+                            isPresented: $isConfirmingSSHKeyReset
+                        ) {
+                            Button("common.reset", role: .destructive) {
+                                resetSSHKey()
+                            }
+                            Button("common.cancel", role: .cancel) {
+                                isConfirmingSSHKeyReset = false
+                            }
+                        }
+                    }
+                }
+
+                Section(
+                    header: Text("source_control.url_specific_credentials"),
+                    footer: Text("credentials.note")
+                ) {
+                    Button("source_control.add_new_credentials") {
+
+                    }
                 }
             }
 
-        }.navigationBarTitle("Authentication", displayMode: .inline)
+        }.navigationBarTitle("source_control.git_authentication", displayMode: .inline)
             .background(Color(.systemGroupedBackground))
             .navigationBarItems(
                 trailing:
-                    Button(NSLocalizedString("Done", comment: "")) {
-                        self.presentationMode.wrappedValue.dismiss()
+                    HStack {
+                        Button(action: {
+                            isDocumentationPresented.toggle()
+                        }) {
+                            Image(systemName: "questionmark.circle")
+                        }
+                        Button("common.done") {
+                            self.presentationMode.wrappedValue.dismiss()
+                        }
                     }
             )
-            .onChange(of: username) { value in
-                KeychainWrapper.standard.set(value, forKey: "git-username")
-                App.workSpaceStorage.gitServiceProvider?.auth(name: value, password: password)
-            }
-            .onChange(of: password) { value in
-                KeychainWrapper.standard.set(value, forKey: "git-password")
-                App.workSpaceStorage.gitServiceProvider?.auth(name: username, password: value)
-            }
             .sheet(isPresented: $isDocumentationPresented) {
                 SafariView(
                     url: URL(
                         string:
                             "https://code.thebaselab.com/guides/version-control#set-up-your-credentials"
                     )!)
-
+            }
+            .onDisappear {
+                if !username.isEmpty {
+                    KeychainWrapper.standard.set(username, forKey: "git-username")
+                } else {
+                    KeychainWrapper.standard.removeObject(forKey: "git-username")
+                }
+                if !password.isEmpty {
+                    KeychainWrapper.standard.set(password, forKey: "git-password")
+                } else {
+                    KeychainWrapper.standard.removeObject(forKey: "git-password")
+                }
+                if !sshKey.isEmpty {
+                    KeychainWrapper.standard.set(sshKey, forKey: "git-ssh-key")
+                } else {
+                    KeychainWrapper.standard.removeObject(forKey: "git-ssh-key")
+                }
             }
     }
 }
