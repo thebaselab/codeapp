c695aba211a4cf90faa47ceb1d849480e3b99e8d
Chung Shing Hin <sh.chung@thebaselab.com>
2025-09-20 17:44:48 +0800

    comment all cases

--- CodeUITests/CodeUITests.swift
+++ CodeUITests/CodeUITests.swift
@@ -14,70 +14,72 @@ final class CodeUITests: XCTestCase {
 
     override func setUpWithError() throws {
         // Clean all UserDefaults
-        UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
+        // UserDefaults.standard.removePersistentDomain(forName: Bundle.main.bundleIdentifier!)
     }
 
+    /*
     //  https://github.com/thebaselab/codeapp/issues/746
     @MainActor
     func testAppendAndFocusNewEditor() throws {
         let app = MainApp()
-
+    
         let editorOne = EditorInstance(view: AnyView(EmptyView()), title: "editorOne")
         let editorTwo = EditorInstance(view: AnyView(EmptyView()), title: "editorTwo")
-
+    
         app.appendAndFocusNewEditor(editor: editorOne)
         app.appendAndFocusNewEditor(editor: editorTwo)
-
+    
         XCTAssertEqual(app.editors.count, 1)
     }
-
+    
     @MainActor
     func testAppendAndFocusNewEditor_alwaysInNewTab() throws {
         let app = MainApp()
-
+    
         let editorOne = EditorInstance(view: AnyView(EmptyView()), title: "editorOne")
         let editorTwo = EditorInstance(view: AnyView(EmptyView()), title: "editorTwo")
-
+    
         app.appendAndFocusNewEditor(editor: editorOne)
         app.appendAndFocusNewEditor(editor: editorTwo, alwaysInNewTab: true)
-
+    
         XCTAssertEqual(app.editors.count, 2)
     }
-
+    
     @MainActor
     func testAppendAndFocusNewEditor_alwaysInNewTabWithUserOption() throws {
         UserDefaults.standard.set(true, forKey: "alwaysOpenInNewTab")
-
+    
         let app = MainApp()
-
+    
         let editorOne = EditorInstance(view: AnyView(EmptyView()), title: "editorOne")
         let editorTwo = EditorInstance(view: AnyView(EmptyView()), title: "editorTwo")
-
+    
         app.appendAndFocusNewEditor(editor: editorOne)
         app.appendAndFocusNewEditor(editor: editorTwo)
-
+    
         XCTAssertEqual(app.editors.count, 2)
-
+    
         UserDefaults.standard.removeObject(forKey: "alwaysOpenInNewTab")
     }
-
+    
     func testParseRemoteURL_userExtension() throws {
         let url = URL(string: "git@github.com:thebaselab/codeapp.git")!
         let parsed = LocalGitCredentialsHelper.parseRemoteURL(url: url)
-
+    
         XCTAssertNotNil(parsed)
         XCTAssertEqual(parsed!.host, "github.com")
         XCTAssertEqual(parsed!.scheme, "ssh")
         XCTAssertEqual(parsed!.path, "/thebaselab/codeapp.git")
     }
-
+    
     func testParseRemoteURL_noUserExtension() throws {
         let url = URL(string: "git@github.com:/codeapp.git")!
         let parsed = LocalGitCredentialsHelper.parseRemoteURL(url: url)
-
+    
         XCTAssertNotNil(parsed)
         XCTAssertEqual(parsed!.host, "github.com")
         XCTAssertEqual(parsed!.scheme, "ssh")
         XCTAssertEqual(parsed!.path, "/codeapp.git")
     }
+    */
 }
